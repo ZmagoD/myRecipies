f1c645ebe41885f8b4991cd6057f3a19b3014777
Zmago Devetak <zmago_devetak@yahoo.com>
2016-01-20 09:15:33 +0000

    finished devise integration

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,6 @@
 class ChefsController < ApplicationController
     before_action :set_chef, only: [ :show ]
-    #before_action :require_same_user, only: [ :edit, :update ]
+
     def index
         @chefs = Chef.paginate( page: params[:page], per_page: 6 )
     end
